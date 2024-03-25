c8c5bc2b94321fb45575f883dea120bf74cd86f7
kamothi <rhkddlf7911@naver.com>
2024-03-25 11:49:38 +0900

    Feat: enum 분리

---
+++ src/main/java/webserver/HttpMethod.java
@@ -0,0 +1,19 @@
+package webserver;
+
+public enum HttpMethod {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE"),
+    PATCH("PATCH");
+
+    String method;
+
+    HttpMethod(String method) {
+        this.method = method;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+}
---
+++ src/main/java/webserver/UrlPath.java
@@ -0,0 +1,21 @@
+package webserver;
+
+public enum UrlPath {
+    INDEX("/index.html"),
+    USER_FORM("/user/form.html"),
+    USER_SIGNUP("/user/signup"),
+    USER_LOGIN("/user/login"),
+    USER_LIST("/user/userList"),
+    ROOT("./webapp"),
+    LOGIN_FAILED("/login_failed.html");
+
+    private final String path;
+
+    UrlPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
