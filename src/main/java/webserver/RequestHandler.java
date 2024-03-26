b2d316322e6936f005b4ad95ba7a2a7ba8357166
hyunn522 <tjgus52213@gmail.com>
2024-03-26 16:33:11 +0900

    feat: index.html 반환 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,8 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static http.util.IOUtils.readData;
 
 public class RequestHandler implements Runnable{
     Socket connection;
@@ -20,7 +27,40 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            // Request header에서 Content-Length
+            // 클라이언트의 요청 메시지를 문자열 형태로 읽어들임 -> 경로에 넣음?
+            // var contentLength = 0;
+            // var message = readData(br, contentLength);
+
+            // InputStream의 요청을 읽어와 파싱
+            String startLine = br.readLine();
+            // String readData = readData(br, startLine.length());
+            String[] startLines = startLine.split(" ");
+            // String requestMethod = startLines[0];
+            String requestUrl = startLines[1];
+
+            String baseUrl = "/Users/seohyun/Desktop/Dev/KUIT3-Backend/KUIT3_Backend-Java-Tomcat/webapp/";
+            String url = "";
+
+            // requestUrl이 빈 값인 경우
+            if(Objects.equals(requestUrl, "/")) {
+                String indexUrl = "index.html";
+                url = baseUrl + indexUrl;
+
+                log.log(Level.INFO, "빈 값, 경로: " + url);
+            }
+            // requestUrl이 존재하는 경우
+            else {
+                url = baseUrl + requestUrl;
+                if(noFile(url)) {
+                    return;
+                }
+                log.log(Level.INFO, "빈 값 아님, 경로: " + url);
+            }
+
+            // 파일이 존재하면 파일 내용을 읽어옴
+            byte[] body = Files.readAllBytes(Paths.get(url));
+
             response200Header(dos, body.length);
             responseBody(dos, body);
 
@@ -47,6 +87,16 @@ public class RequestHandler implements Runnable{
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
+    }
+
+    private boolean noFile(String url){
+        // file이 존재하는지 확인
+        File file = new File(url);
+        if (!file.exists()) {
+            // 파일이 존재하지 않으면 true 반환
+            return true;
+        }
+        return false;
     }
 
 }
