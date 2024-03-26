da0437396e84c9926e7f78d02b09096255374386
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-03-26 10:55:16 +0900

    Feat: index.html 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,30 +2,36 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class RequestHandler implements Runnable{
+
+
+public class RequestHandler implements Runnable {
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
-    }
+    } //클라이언트와 연결을 받아들이 소켓을 인스턴스 변수에 할당
 
     @Override
-    public void run() {
+    public void run() { //클라이언트 요청 처리
+        //클라이언트 연결 정보 기록
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            Path path = new File("./webapp/index.html").toPath();
+            byte[] body = Files.readAllBytes(path);
             response200Header(dos, body.length);
             responseBody(dos, body);
 
         } catch (IOException e) {
-            log.log(Level.SEVERE,e.getMessage());
+            log.log(Level.SEVERE, e.getMessage());
         }
     }
 
