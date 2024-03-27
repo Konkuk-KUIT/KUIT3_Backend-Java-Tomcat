17abf5c8c649ff0d59ca3d34610673642b6feb93
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-03-27 14:50:59 +0900

    feat: 요구사항 1: index.html 반환하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,13 +2,17 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
-
+    private static final String ROOT_URL = "./webapp";
+    private static final String HOME_URL = "/index.html";
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
@@ -20,7 +24,25 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            //TODO
+            // /r/n으로 분리되어 있지 않은 body의 내용은 꼭! IOUtils에 있는 것 처럼
+            // contentLength와 함께 넘겨주어 읽어야만 정확히 데이터를 읽어올 수 있다.
+            // contentLength를 어떻게 알죠....?
+            String startLine = br.readLine();
+            String[] startLines = startLine.split(" ");
+            String method = startLines[0];
+            String url = startLines[1];
+            log.log(Level.INFO, "method: " + method + ", url: " + url);
+            // 정보: method: GET, url: /
+            // 정보: method: GET, url: /favicon.ico -> favicon은 브라우저에 띄울 웹사이트 아이콘으로 브라우저에서 요청!
+
+            //byte[] body = "Hello World".getBytes();
+            byte[] body = new byte[0];
+
+            if(url.equals("/") || method.equals("GET") && url.equals("/index.html")) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL + HOME_URL));
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
 
