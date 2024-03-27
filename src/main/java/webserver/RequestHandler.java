af3b8c47184d0c223b42c8046b5d3bbd577faacd
김민석 <skyfox001015@naver.com>
2024-03-27 21:34:37 +0900

    feat : 요구사항 1구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,11 +2,22 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+
 public class RequestHandler implements Runnable{
+
     Socket connection;
+    private static final String ROOT_URL = "./webapp";
+    private static final String HOME_URL = "/index.html";
+
+    private final Path homePath = Paths.get(ROOT_URL + HOME_URL);
+
+
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
     public RequestHandler(Socket connection) {
@@ -15,12 +26,35 @@ public class RequestHandler implements Runnable{
 
     @Override
     public void run() {
+        //try-with-resources 구문사용
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
+            byte[] body = new byte[0];
+
+            //서버로 부터 오는 Header 분석하는 부분 //////////
+            String startLine = br.readLine();
+            String[] startLines = startLine.split(" ");
+            //startLines : ["GET", "/", "HTTP/1.1"]
+            String method = startLines[0];
+            //method : "GET"
+            String url = startLines[1];
+            //url : "/"
+
+            //서버에서 요청이 GET이고 .html로 끝난다면
+            if (method.equals("GET") && url.endsWith(".html")) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL + url));
+            }
+
+            if (url.equals("/")) {
+                body = Files.readAllBytes(homePath);
+            }
+
+
+
+
             response200Header(dos, body.length);
             responseBody(dos, body);
 
