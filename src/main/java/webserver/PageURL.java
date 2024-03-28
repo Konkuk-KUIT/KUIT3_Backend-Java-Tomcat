38b5392dc34a35c8d9217fb50d95b34ccecded5a
AppleMint98 <hong721816@konkuk.ac.kr>
2024-03-28 18:18:36 +0900

    0328

---
+++ src/main/java/webserver/PageURL.java
@@ -0,0 +1,25 @@
+package webserver;
+
+public enum PageURL {
+    ROOT("./webapp"),
+    HOME("/index.html"),
+    LOGIN_FAILED("/user/login_failed.html"),
+    LOGIN("/user/login.html"),
+    LIST("/user/list.html");
+
+    private final String url;
+
+    PageURL(String url) {
+        this.url = url;
+    }
+    public String getUrl() {
+        return url;
+    }
+
+    public String getFullPath() {
+        if (this == ROOT) {
+            return url;
+        }
+        return ROOT.url + url;
+    }
+}
---
+++ src/main/java/webserver/controller/ForwardController.java
@@ -0,0 +1,11 @@
+package webserver;
+
+import java.io.IOException;
+
+public class ForwardController implements Controller {
+
+    @Override
+    public void execute(HttpRequest req, HttpResponse res) throws IOException {
+
+    }
+}
