91f1290c76569df809aa77ae17b85561a0e47a55
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-03-28 16:51:23 +0900

    Feat: 요구사항 2,3,4 구현중

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,32 +1,85 @@
 package webserver;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
+import db.MemoryUserRepository;
+import db.Repository;
+import http.util.HttpRequestUtils;
+import http.util.IOUtils;
+import model.User;
 
 public class RequestHandler implements Runnable {
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private static final String ROOT_URL = "./webapp";
+    private static final String HOME_URL = "/index.html";
+    private final Repository repository;
+    private final Path homePath = Paths.get(ROOT_URL + HOME_URL);
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
+        this.repository = MemoryUserRepository.getInstance()
+        ;
     } //클라이언트와 연결을 받아들이 소켓을 인스턴스 변수에 할당
 
     @Override
-    public void run() { //클라이언트 요청 처리
-        //클라이언트 연결 정보 기록
-        log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
+    public void run() { // 클라이언트 요청 처리
+        // 클라이언트 연결 정보 기록
+        log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress()
+                + ", Port : " + connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            Path path = new File("./webapp/index.html").toPath();
-            byte[] body = Files.readAllBytes(path);
+            byte[] body = new byte[0];
+
+            String requestLine = br.readLine();
+            String[] requestLines = requestLine.split(" ");
+            String method = requestLines[0];
+            String url = requestLines[1];
+
+            int requestContentLength = 0;
+
+            while (true) {
+                final String line = br.readLine();
+                if (line.equals("")) {
+                    break;
+                }
+                if (line.startsWith("Content-Length")) {
+                    requestContentLength = Integer.parseInt(line.split(":")[1]);
+                }
+            }
+
+            if (method.equals("GET") && url.equals("/")) {
+                body = Files.readAllBytes(homePath);;
+            }
+
+            if (method.equals("GET") && url.endsWith(".html")) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL + url));
+            }
+
+            if (url.equals("/user/signup")) {
+                // readBody 메소드에 contentLength, BufferedReader를 함께 보내어 body 값을 읽을 수 있을 것
+                String queryString = IOUtils.readData(br, requestContentLength);
+                // 새로운 user 인스턴스 생성, MemoryUserRepository에 저장
+                Map<String, String> queryParameter = HttpRequestUtils.parseQueryParameter(queryString);
+                User user = new User(queryParameter.get("userId"), queryParameter.get("password"),
+                        queryParameter.get("name"), queryParameter.get("email"));
+                repository.addUser(user);
+
+                response302Header(dos, HOME_URL);
+                return;
+            }
+
             response200Header(dos, body.length);
             responseBody(dos, body);
 
@@ -41,6 +94,31 @@ public class RequestHandler implements Runnable {
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n"); // 상태코드 변경
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response404NotFound(DataOutputStream dos) {
+        try {
+            String notFoundMessage = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>";
+            byte[] notFoundBody = notFoundMessage.getBytes("UTF-8");
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + notFoundBody.length + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.write(notFoundBody, 0, notFoundBody.length);
+            dos.flush();
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
