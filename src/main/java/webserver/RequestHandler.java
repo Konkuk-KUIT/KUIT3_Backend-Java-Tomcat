8f27d89e84bb9b4e17cbfbb9ee1d93e527fe9733
rlawngP91 <annie2104@naver.com>
2024-03-29 19:28:52 +0900

    feat : Login 기능

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -68,29 +68,6 @@ public class RequestHandler implements Runnable{
                 int requestContentLength = 0;
                 while (true) {
                     final String line = br.readLine();
-                    //log.log(Level.INFO,"check point : " + line);
-                    //***
-                    //Host: localhost
-                    //Connection: keep-alive
-                    //Content-Length: 77
-                    //Cache-Control: max-age=0
-                    //sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"
-                    //sec-ch-ua-mobile: ?0
-                    //sec-ch-ua-platform: "macOS"
-                    //Upgrade-Insecure-Requests: 1
-                    //Origin: http://localhost
-                    //Content-Type: application/x-www-form-urlencoded
-                    //User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
-                    //Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
-                    //Sec-Fetch-Site: same-origin
-                    //Sec-Fetch-Mode: navigate
-                    //Sec-Fetch-User: ?1
-                    //Sec-Fetch-Dest: document
-                    //Referer: http://localhost/user/form.html
-                    //Accept-Encoding: gzip, deflate, br, zstd
-                    //Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
-                    //Cookie: _xsrf=2|b0db7321|d7db5fdf21657b9746bdf8d95f1d1c4b|1709786747; username-localhost-8888="2|1:0|10:1711000933|23:username-localhost-8888|44:YjdiNzg1Njg4MjAyNDZjOThjMDNlMjJmY2MwNmNjZGI=|c08046ec9f5629558200765478cc011a4706d563a76fa8a94e3b4625d00ffe2c"
-                    //***
                     if (line.equals("")) {
                         break;
                     }
@@ -110,17 +87,70 @@ public class RequestHandler implements Runnable{
                 db.addUser(user);
 
                 //redirect 시키기
+                //클라이언트가 요청하는 url..?
                 response302Header(dos, "/index.html");
+            }
+            if (method.equals("GET") && url.equals("/user/login.html")){
+                File loginFile = new File("webapp/user/login.html");
+                byte[] body = Files.readAllBytes(loginFile.toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+            if (method.equals("POST") && url.equals("/user/login")){
+                int requestContentLength = 0;
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.equals("")) {
+                        break;
+                    }
+                    // header info
+                    if (line.startsWith("Content-Length")) {
+                        requestContentLength = Integer.parseInt(line.split(": ")[1]);
+                    }
+                }
+                String loginUserInfo = IOUtils.readData(br, requestContentLength);
+                String[] loginUserInfos = loginUserInfo.split("&");
+                String[] userIdInfo = loginUserInfos[0].split("=");
+                String[] userPasswordInfo = loginUserInfos[1].split("=");
+
+                MemoryUserRepository getDB = MemoryUserRepository.getInstance();
+                User loginuser = getDB.findUserById(userIdInfo[1]);
+                //user 동일함
+                if ((loginuser != null) && loginuser.getPassword().equals(userPasswordInfo[1])){
+                    response302HeaderWithCookie(dos, "/index.html");
+                }
+                response302Header(dos, "login_failed.html");
+            }
+            if (method.equals("GET") && url.equals("/user/login_failed.html")){
+                File loginFailFile = new File("webapp/user/login_failed.html");
+                byte[] body = Files.readAllBytes(loginFailFile.toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
             }
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
     }
+
+    private void response302HeaderWithCookie(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + path +"\r\n");
+            //Cookie: logined=true
+            dos.writeBytes("Set-Cookie: logined=true; Path=/;\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
     private void response302Header(DataOutputStream dos, String path){
         try {
             dos.writeBytes("HTTP/1.1 302 Found \r\n");
             dos.writeBytes("Location: " + path);
             dos.writeBytes("\r\n");
+            dos.flush();
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
@@ -144,5 +174,27 @@ public class RequestHandler implements Runnable{
             log.log(Level.SEVERE, e.getMessage());
         }
     }
-
+    //log.log(Level.INFO,"check point : " + line);
+    //***
+    //Host: localhost
+    //Connection: keep-alive
+    //Content-Length: 77
+    //Cache-Control: max-age=0
+    //sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"
+    //sec-ch-ua-mobile: ?0
+    //sec-ch-ua-platform: "macOS"
+    //Upgrade-Insecure-Requests: 1
+    //Origin: http://localhost
+    //Content-Type: application/x-www-form-urlencoded
+    //User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
+    //Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+    //Sec-Fetch-Site: same-origin
+    //Sec-Fetch-Mode: navigate
+    //Sec-Fetch-User: ?1
+    //Sec-Fetch-Dest: document
+    //Referer: http://localhost/user/form.html
+    //Accept-Encoding: gzip, deflate, br, zstd
+    //Accept-Language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7
+    //Cookie: _xsrf=2|b0db7321|d7db5fdf21657b9746bdf8d95f1d1c4b|1709786747; username-localhost-8888="2|1:0|10:1711000933|23:username-localhost-8888|44:YjdiNzg1Njg4MjAyNDZjOThjMDNlMjJmY2MwNmNjZGI=|c08046ec9f5629558200765478cc011a4706d563a76fa8a94e3b4625d00ffe2c"
+    //***
 }
