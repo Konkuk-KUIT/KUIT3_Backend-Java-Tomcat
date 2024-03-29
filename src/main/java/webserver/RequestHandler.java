926147f5b17fa4550b5da5596031f6497ebe28e1
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-03-29 13:37:02 +0900

    feat: 요구사항 2: GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,21 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import db.Repository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static http.util.HttpRequestUtils.parseQueryParameter;
 
 public class RequestHandler implements Runnable{
     Socket connection;
@@ -24,10 +33,6 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            //TODO
-            // /r/n으로 분리되어 있지 않은 body의 내용은 꼭! IOUtils에 있는 것 처럼
-            // contentLength와 함께 넘겨주어 읽어야만 정확히 데이터를 읽어올 수 있다.
-            // contentLength를 어떻게 알죠....?
             String startLine = br.readLine();
             String[] startLines = startLine.split(" ");
             String method = startLines[0];
@@ -40,6 +45,18 @@ public class RequestHandler implements Runnable{
             byte[] body = new byte[0];
 
             if(url.equals("/") || method.equals("GET") && url.equals("/index.html")) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL + HOME_URL));
+            }
+
+            if(method.equals("GET") && url.equals("/user/form.html")) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL + url));
+            }
+
+            if(method.equals("GET") && url.startsWith("/user/signup?")) {
+                Map<String, String> map = parseQueryParameter(url.substring("/user/signup?".length()));
+                Repository repository = MemoryUserRepository.getInstance();
+                repository.addUser(new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email")));
+                log.log(Level.INFO, "saved " + repository.findUserById(map.get("userId")).toString());
                 body = Files.readAllBytes(Paths.get(ROOT_URL + HOME_URL));
             }
 
